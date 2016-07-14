31cb02c367dfab1bb665fd8997fa537f014d71a0
Edward Nolan <eddiejnolan@gmail.com>
2016-07-14 12:11:53 -0400

    more use-package; moved and indented code

--- init.el
+++ init.el
@@ -5,68 +5,6 @@
 ;; col numbers
 (setq column-number-mode t)
 
-;; tabs
-(setq-default indent-tabs-mode nil)
-(setq c-basic-offset 2)
-(setq js-indent-level 2)
-(setq-default tab-width 2)
-(setq-default tab-stop-list (number-sequence 2 200 2))
-
-;; file backups
-;; prevent emacs from making backups the usual way
-(setq backup-inhibited t)
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-;; use backup-each-save
-(add-to-list 'load-path "~/.emacs.d/elpa/backup-each-save-20130704.732/")
-(require 'backup-each-save)
-(add-hook 'after-save-hook 'backup-each-save)
-
-;; meta-arrow to move between buffers
-(global-set-key [M-left] 'windmove-left)
-(global-set-key [M-right] 'windmove-right)
-(global-set-key [M-up] 'windmove-up)
-(global-set-key [M-down] 'windmove-down)
-
-;; major mode hooks
-;; delete trailing whitespace; turn off electric indent
-;; C
-(add-hook 'c-mode-hook
-                (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-(add-hook 'c-mode-hook (lambda () (electric-indent-local-mode -1)))
-(add-hook 'c-mode-hook
-          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
-(add-hook 'c-mode-hook (lambda () (local-set-key (kbd "TAB") 'tab-to-tab-stop)))
-;; C++
-(add-hook 'c++-mode-hook
-                (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-(add-hook 'c++-mode-hook (lambda () (electric-indent-local-mode -1)))
-(add-hook 'c++-mode-hook
-          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
-(add-hook 'c++-mode-hook (lambda () (local-set-key (kbd "TAB") 'tab-to-tab-stop)))
-;; OCaml
-(add-hook 'tuareg-mode-hook
-                (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-(add-hook 'tuareg-mode-hook (lambda () (electric-indent-local-mode -1)))
-(add-hook 'tuareg-mode-hook
-          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
-;; Emacs Lisp
-(add-hook 'emacs-lisp-mode-hook
-                (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-(add-hook 'emacs-lisp-mode-hook (lambda () (electric-indent-local-mode -1)))
-(add-hook 'emacs-lisp-mode-hook
-          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
-(add-hook 'emacs-lisp-mode-hook (lambda () (local-set-key (kbd "TAB") 'tab-to-tab-stop)))
-
-;; annoying messages
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-echo-area-message "eddie")
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-;; cursor
-(setq-default cursor-type 'bar)
-
 ;; line numbers
 (global-linum-mode t)
 (defadvice linum-update-window (around linum-dynamic activate)
@@ -74,6 +12,28 @@
                      (count-lines (point-min) (point-max)))))
          (linum-format (concat " %" (number-to-string w) "d ")))
     ad-do-it))
+
+;; tabs
+(setq-default indent-tabs-mode nil)
+(setq c-basic-offset 2)
+(setq js-indent-level 2)
+(setq-default tab-width 2)
+(setq-default tab-stop-list (number-sequence 2 200 2))
+
+;; meta-arrow to move between buffers
+(global-set-key [M-left] 'windmove-left)
+(global-set-key [M-right] 'windmove-right)
+(global-set-key [M-up] 'windmove-up)
+(global-set-key [M-down] 'windmove-down)
+
+;; cursor
+(setq-default cursor-type 'bar)
+
+;; annoying startup messages
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-echo-area-message "eddie")
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
 
 ;; delete selection mode
 (delete-selection-mode 1)
@@ -86,8 +46,40 @@
                     nil
                     :foreground "gray")
 
-;; package management
+;; major mode hooks
+;; typically:
+;; delete trailing whitespace
+;; turn off electric indent
+;; C-j for newline-and-indent
+;; tab key only inserts spaces to tab stop
+;; C
+(add-hook 'c-mode-hook
+          (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(add-hook 'c-mode-hook (lambda () (electric-indent-local-mode -1)))
+(add-hook 'c-mode-hook
+          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
+(add-hook 'c-mode-hook (lambda () (local-set-key (kbd "TAB") 'tab-to-tab-stop)))
+;; C++
+(add-hook 'c++-mode-hook
+          (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(add-hook 'c++-mode-hook (lambda () (electric-indent-local-mode -1)))
+(add-hook 'c++-mode-hook
+          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
+(add-hook 'c++-mode-hook (lambda () (local-set-key (kbd "TAB") 'tab-to-tab-stop)))
+;; OCaml
+(add-hook 'tuareg-mode-hook
+          (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(add-hook 'tuareg-mode-hook (lambda () (electric-indent-local-mode -1)))
+(add-hook 'tuareg-mode-hook
+          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
+;; Emacs Lisp
+(add-hook 'emacs-lisp-mode-hook
+          (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (electric-indent-local-mode -1)))
+(add-hook 'emacs-lisp-mode-hook
+          (lambda () (local-set-key (kbd "C-j") #'newline-and-indent)))
 
+;; package management
 ;; melpa
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -96,20 +88,27 @@
    '("melpa" . "http://melpa.org/packages/")
    t)
   (package-initialize))
-
 ;; use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (eval-when-compile
   (require 'use-package))
 
+;; file backups
+(use-package backup-each-save
+  :config
+  (setq backup-inhibited t)
+  (setq auto-save-default nil)
+  (setq make-backup-files nil)
+  (add-hook 'after-save-hook 'backup-each-save)
+  )
+
 ;; cleaner mode lines
-(require 'diminish)
+(use-package diminish)
 
 ;; helper for mode remappings
-(require 'bind-key)
+(use-package bind-key)
 
 ;; programming languages
 
@@ -122,64 +121,55 @@
   :config
   (use-package company-irony :ensure t :defer t)
   (setq company-idle-delay              nil
-	company-minimum-prefix-length   2
-	company-show-numbers            t
-	company-tooltip-limit           20
-	company-dabbrev-downcase        nil
-	company-backends                '((company-irony company-gtags))
-	)
+        company-minimum-prefix-length   2
+        company-show-numbers            t
+        company-tooltip-limit           20
+        company-dabbrev-downcase        nil
+        company-backends                '((company-irony company-gtags))
+        )
   :bind ("C-;" . company-complete-common)
   )
 
 ;; ocaml
 ;; -- common-lisp compatibility if not added earlier in your .emacs
 (require 'cl)
-
-;; -- Tuareg mode -----------------------------------------
-;; Add Tuareg to your search path
-(load "/home/eddie/.opam/4.03.0/share/emacs/site-lisp/tuareg-site-file")
-(require 'tuareg)
-(setq auto-mode-alist
-      (append '(("\\.ml[ily]?$" . tuareg-mode))
-          auto-mode-alist))
-
-;; -- opam and utop setup --------------------------------
 ;; Setup environment variables using opam
 (dolist
-   (var (car (read-from-string
-           (shell-command-to-string "opam config env --sexp"))))
- (setenv (car var) (cadr var)))
+    (var (car (read-from-string
+               (shell-command-to-string "opam config env --sexp"))))
+  (setenv (car var) (cadr var)))
 ;; Update the emacs path
 (setq exec-path (split-string (getenv "PATH") path-separator))
 ;; Update the emacs load path
 (push (concat (getenv "OCAML_TOPLEVEL_PATH")
-          "/../../share/emacs/site-lisp") load-path)
-;; merlin
-     ;; Add opam emacs directory to the load-path
-     (setq opam-share (substring (shell-command-to-string "opam config var
+              "/../../share/emacs/site-lisp") load-path)
+;; Add opam emacs directory to the load-path
+(setq opam-share (substring (shell-command-to-string "opam config var
    share 2> /dev/null") 0 -1))
-     (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-     ;; Load merlin-mode
-     (require 'merlin)
-     ;; Start merlin on ocaml files
-     (add-hook 'tuareg-mode-hook 'merlin-mode t)
-     (add-hook 'caml-mode-hook 'merlin-mode t)
-     ;; Enable auto-complete
-     (setq merlin-use-auto-complete-mode 'easy)
-     ;; Use opam switch to lookup ocamlmerlin binary
-     (setq merlin-command 'opam)
-; Make company aware of merlin
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+;; Tuareg
+(require 'tuareg)
+(setq auto-mode-alist
+      (append '(("\\.ml[ily]?$" . tuareg-mode))
+              auto-mode-alist))
+;; Merlin
+(require 'merlin)
+;; Start merlin on ocaml files
+(add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'caml-mode-hook 'merlin-mode t)
+;; Enable auto-complete
+(setq merlin-use-auto-complete-mode 'easy)
+;; Use opam switch to lookup ocamlmerlin binary
+(setq merlin-command 'opam)
+;; Make company aware of merlin
 (with-eval-after-load 'company
- (add-to-list 'company-backends 'merlin-company-backend))
-
-; bind merlin-locate to C-c ;
+  (add-to-list 'company-backends 'merlin-company-backend))
+;; bind merlin-locate to C-c ;
 (add-hook 'merlin-mode-hook (lambda () (local-set-key (kbd "C-c ;") 'merlin-locate)))
-
-; use ocp-indent
+;; use ocp-indent
 (require 'ocp-indent)
 
 ;; C++
-
 ;; == irony-mode ==
 (use-package irony
   :ensure t
