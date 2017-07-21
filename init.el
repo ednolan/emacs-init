7f9a4d4f5da1810ae00dc3a717e172308add5bac
Edward Nolan <eddiejnolan@gmail.com>
2017-07-20 20:51:49 -0400

    other font

--- init.el
+++ init.el
@@ -196,4 +196,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Menlo" :foundry "PfEd" :slant normal :weight normal :height 109 :width normal)))))
