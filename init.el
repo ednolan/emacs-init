4fde8d581d76fb5c7f9b9cf6c8ee3b33647bb0aa
Edward Nolan <eddiejnolan@gmail.com>
2017-08-02 19:00:48 -0400

    xetex

--- init.el
+++ init.el
@@ -156,14 +156,17 @@
 (use-package tex-mode
   :ensure auctex
   :defer t
-)
-(use-package auctex-latexmk
-  :ensure t
-  :defer t
   :init
-  (auctex-latexmk-setup)
-  (setq auctex-latexmk-inherit-TeX-PDF-mode t)
+  (setq-default TeX-engine 'xetex)
+  (setq-default TeX-PDF-mode t)
 )
+;; set XeTeX mode in TeX/LaTeX
+(add-hook 'LaTeX-mode-hook
+          (lambda()
+            (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+            (setq TeX-command-default "XeLaTeX")
+            (setq TeX-save-query nil)
+            (setq TeX-show-compilation t)))
 
 ;; OCaml
 ;; tuareg
