e2bd40a0bc0e1b372a18f0ad8cb03764b602965d
Edward Nolan <eddiejnolan@gmail.com>
2017-09-26 14:27:05 -0400

    irony

--- init.el
+++ init.el
@@ -207,6 +207,32 @@
 ;; helper for mode remappings
 (use-package bind-key)
 
+;; fix mac os path issue
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (push "HISTFILE" exec-path-from-shell-variables)
+  (setq exec-path-from-shell-check-startup-files nil)
+  (exec-path-from-shell-initialize))
+
+;; irony
+(use-package irony
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  :config
+  ;; replace the `completion-at-point' and `complete-symbol' bindings in
+  ;; irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  )
+
 ;; Custom
 
 (custom-set-variables
@@ -216,7 +242,6 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(linum-format (quote dynamic))
- '(package-selected-packages (quote (use-package)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
