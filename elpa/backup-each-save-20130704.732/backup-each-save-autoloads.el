085c33d505043d56901e418b593143bf31e4d0a6
Edward Nolan <edward@manatechllc.com>
2018-04-04 22:29:39 -0400

    get backup-each-save from melpa not git

--- elpa/backup-each-save-20130704.732/backup-each-save-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; backup-each-save-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (backup-each-save) "backup-each-save" "backup-each-save.el"
-;;;;;;  (22316 20391 509760 757000))
-;;; Generated autoloads from backup-each-save.el
-
-(autoload 'backup-each-save "backup-each-save" "\
-
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("backup-each-save-pkg.el") (22316 20391
-;;;;;;  527612 382000))
-
-;;;***
-
-(provide 'backup-each-save-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; backup-each-save-autoloads.el ends here
