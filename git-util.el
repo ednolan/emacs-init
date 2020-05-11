3c6d7ce3689419a5316952eaa541541e7fca4858
Edward Nolan <enolan@maystreet.com>
2020-05-11 19:11:47 -0400

    Git-util update

--- git-util.el
+++ git-util.el
@@ -1,7 +1,14 @@
-(defun helm-git-grep-this-project ()
+(defun git-util-this-project-paths ()
+  (split-string (shell-command-to-string
+                          (concat "cd " default-directory " ; git diff --name-status master | cut -f2"))))
+
+(defun git-util-open-this-project ()
+  "Open all paths that differ from master in this git repository"
+  (interactive)
+  (mapc 'find-file-noselect (git-util-this-project-paths)))
+
+(defun git-util-helm-git-grep-this-project ()
   "Perform a helm git grep with only the paths that differ from master"
   (interactive)
-  (let* ((modified-files (split-string (shell-command-to-string
-                          (concat "cd " default-directory " ; git diff --name-status master | cut -f2"))))
-         (helm-git-grep-pathspecs modified-files))
+  (let ((helm-git-grep-pathspecs (git-util-this-project-paths)))
     (helm-git-grep)))
