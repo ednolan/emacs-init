ce9372793bcb3b2070d7098f6dccef70188dd954
Edward Nolan <enolan@maystreet.com>
2020-05-12 23:52:30 -0400

    git-util

--- git-util.el
+++ git-util.el
@@ -1,6 +1,15 @@
 (defun git-util-this-project-paths ()
   (split-string (shell-command-to-string
-                          (concat "cd " default-directory " ; git diff --name-status master | cut -f2"))))
+                 (concat "cd " default-directory " ; git diff --name-status master | cut -f2 | xargs -n1 -i{} echo $(git rev-parse --show-toplevel)/{}"))))
+
+(defun git-util-this-repo-paths ()
+  (split-string (shell-command-to-string
+                 (concat "cd " default-directory " ; git ls-tree --full-name --full-tree -r --name-only HEAD | xargs -n1 -i{} echo $(git rev-parse --show-toplevel)/{}"))))
+
+(defun git-util-open-this-repo ()
+  "Open all paths in this git repository"
+  (interactive)
+  (mapc 'find-file-noselect (git-util-this-repo-paths)))
 
 (defun git-util-open-this-project ()
   "Open all paths that differ from master in this git repository"
